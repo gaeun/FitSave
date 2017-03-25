9ef019b15900bff736e8f4dceab38b6867c3de3c
gaeun <gaeunkim@ohs.stanford.edu>
2017-03-25 14:26:17 -0700

    Start on dashboard

--- FitSave/ViewController.swift
+++ FitSave/ViewController.swift
@@ -7,39 +7,75 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myTableView: UITableView  =   UITableView()
+    var itemsToLoad: [String] = ["Nike", "Chipotle", "Boething"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        getHealthKitPermission()
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        self.view.backgroundColor = UIColor(red:0.62, green:0.86, blue:0.62, alpha:1.0)
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight*0.065))
+        label.backgroundColor = UIColor(red:0.41, green:0.55, blue:0.41, alpha:1.0)
+        label.center = CGPoint(x: screenWidth*0.5, y: screenHeight*0.47)
+        label.textAlignment = .center
+        label.text = "Deals Near You"
+        label.textColor = UIColor.white
+        self.view.addSubview(label)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    
-    func getHealthKitPermission() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        // Seek authorization in HealthKitManager.swift.
-        healthManager.authorizeHealthKit { (authorized,  error) -> Void in
-            if authorized {
-                
-                // Get and set the user's height.
-                self.setHeight()
-            } else {
-                if error != nil {
-                    print(error)
-                }
-                print("Permission denied.")
-            }
-        }
+        // Get main screen bounds
+        let screenSize: CGRect = UIScreen.main.bounds
+        
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        myTableView.frame = CGRectMake(0, screenHeight*0.5, screenWidth, screenHeight*0.5);
+        myTableView.dataSource = self
+        myTableView.delegate = self
+        
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        
+        self.view.addSubview(myTableView)
+        
     }
-
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return itemsToLoad.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        cell.textLabel?.text = self.itemsToLoad[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        print("User selected table row \(indexPath.row) and item \(itemsToLoad[indexPath.row])")
+    }
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
 }
-
