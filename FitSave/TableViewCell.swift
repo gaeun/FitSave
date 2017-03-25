8f73e8933d3eabf50d8a778511d86c344a4bdda9
gaeun <gaeunkim@ohs.stanford.edu>
2017-03-25 16:17:47 -0700

    Add things to dashboard

---
+++ FitSave/TableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  TableViewCell.swift
+//  FitSave
+//
+//  Created by Gaeun Kim on 3/25/17.
+//  Copyright © 2017 HSHacks Team. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    var myLabel1: UILabel!
+    var myLabel2: UILabel!
+    var brandImage: UIImage!
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:)")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        let gap : CGFloat = 13
+        let labelHeight: CGFloat = 30
+        let labelWidth: CGFloat = 150
+        let lineGap : CGFloat = 5
+        let label2Y : CGFloat = gap + labelHeight
+        let imageSizeX : CGFloat = 105
+        let imageSizeY : CGFloat = 61
+        
+        myLabel1 = UILabel()
+        myLabel1.frame = CGRectMake(imageSizeX+gap*2, gap, labelWidth, labelHeight)
+        myLabel1.textColor = UIColor.black
+        contentView.addSubview(myLabel1)
+        
+        myLabel2 = UILabel()
+        myLabel2.frame = CGRectMake(imageSizeX+gap*2, label2Y, labelWidth, labelHeight)
+        myLabel2.textColor = UIColor.black
+        contentView.addSubview(myLabel2)
+        
+        brandImage = UIImage(named: "NikeLogo")!
+        let BrandImageView = UIImageView(image: brandImage)
+        BrandImageView.frame = CGRectMake(gap, gap, imageSizeX, imageSizeY)
+        contentView.addSubview(BrandImageView)
+    }
+    
+    func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+}
--- FitSave/ViewController.swift
+++ FitSave/ViewController.swift
@@ -11,6 +11,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     var myTableView: UITableView  =   UITableView()
     var itemsToLoad: [String] = ["Nike", "Chipotle", "Boething"]
+    var fitnessPointsArray: [String] = ["75,000 points", "25,000 points", "10,000 points"]
+    
+    var logoImage: [UIImage] = [
+        UIImage(named: "NikeLogo")!,
+        UIImage(named: "ChipotleLogo")!,
+        UIImage(named: "BoethingLogo")!
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +26,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let screenSize: CGRect = UIScreen.main.bounds
         
         let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
         
         self.view.backgroundColor = UIColor(red:0.62, green:0.86, blue:0.62, alpha:1.0)
         
-        let label = UILabel(frame: CGRect(x: 0, y: 0, width: screenWidth, height: screenHeight*0.065))
+        // Deals Near You Label
+        let label = UILabel(frame: CGRect(x: 0, y: 252, width: screenWidth, height: 40))
         label.backgroundColor = UIColor(red:0.41, green:0.55, blue:0.41, alpha:1.0)
-        label.center = CGPoint(x: screenWidth*0.5, y: screenHeight*0.47)
         label.textAlignment = .center
         label.text = "Deals Near You"
         label.textColor = UIColor.white
         self.view.addSubview(label)
+        
+        // Images
+        let CaloriesImage: UIImage = UIImage(named: "CaloriesIcon")!
+        let CaloriesImageView = UIImageView(image: CaloriesImage)
+        self.view.addSubview(CaloriesImageView)
+        CaloriesImageView.frame = CGRectMake(46, 101, 25, 28)
+        
+        let OvalImage: UIImage = UIImage(named: "Oval")!
+        let OvalImageView = UIImageView(image: OvalImage)
+        self.view.addSubview(OvalImageView)
+        OvalImageView.frame = CGRectMake(113, 51, 150, 150)
+        
+        let StepsImage: UIImage = UIImage(named: "StepsIcon")!
+        let StepsImageView = UIImageView(image: StepsImage)
+        self.view.addSubview(StepsImageView)
+        StepsImageView.frame = CGRectMake(301, 98, 36, 28)
+        //
+        
+        // Labels in header
+        let fitnessPointsLabel = UILabel(frame: CGRect(x: 134, y: 128, width: 108, height: 24))
+        fitnessPointsLabel.textAlignment = .center
+        fitnessPointsLabel.text = "fitness points"
+        fitnessPointsLabel.textColor = UIColor.white
+        self.view.addSubview(fitnessPointsLabel)
     }
     
     override func didReceiveMemoryWarning() {
@@ -46,7 +76,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let screenWidth = screenSize.width
         let screenHeight = screenSize.height
         
-        myTableView.frame = CGRectMake(0, screenHeight*0.5, screenWidth, screenHeight*0.5);
+        myTableView.frame = CGRectMake(0, 292, screenWidth, screenHeight-292);
         myTableView.dataSource = self
         myTableView.delegate = self
         
@@ -56,6 +86,9 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
@@ -64,10 +97,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        
-        cell.textLabel?.text = self.itemsToLoad[indexPath.row]
-        
+        let cell = TableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "myIdentifier")
+        cell.myLabel1.text = itemsToLoad[indexPath.row]
+        cell.myLabel2.text = fitnessPointsArray[indexPath.row]
+        cell.brandImage = logoImage[indexPath.row]
         return cell
     }
     
@@ -78,4 +111,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func CGRectMake(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> CGRect {
         return CGRect(x: x, y: y, width: width, height: height)
     }
+    
 }
